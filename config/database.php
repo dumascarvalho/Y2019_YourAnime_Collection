ca9c89f08d23956007c2fb37f68c4f71aa59ab2a
Dumas Morais de Carvalho <dumasmcarvalho@gmail.com>
2019-06-06 01:07:46 -0300

    Initial Commit

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+function openConnection($DB)
+{
+    $DB_HOST = "localhost:3307";
+    $DB_USER = "root";
+    $DB_PASS = "";
+
+    $CONN = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB);
+
+    if ($CONN->connect_error) {
+        die("Não foi possível estabelecer conexão: " . $CONN->connect_error);
+    } else {
+        echo "Conexão realizada com sucesso!";
+    }
+
+    return $CONN;
+}
+
+function closeConnection($CONN)
+{
+    $CONN->close();
+    echo "<br>Conexão encerrada com sucesso!";
+}
